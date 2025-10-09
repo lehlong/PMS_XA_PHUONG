f357b5f55187085611293d1674ca08acfdb92adf
Hải Long <hlonn.it@gmail.com>
2025-10-09 17:12:22 +0700

    .

---
+++ Project.Core/Entities/PS/PsProject.cs
@@ -0,0 +1,29 @@
+﻿using Project.Core.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project.Core.Entities.PS
+{
+    public class PsProject : SoftDeleteEntity
+    {
+        [Key]
+        public string Id { get; set; } = string.Empty;
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public string? DonViPhuTrach { get; set; }
+        public string? LanhDaoPhuTrach { get; set; }
+        public string? LoaiDuAn { get; set; }
+        public string? PmDuAn { get; set; }
+        public string? CapDuAn { get; set; }
+        public string? PhuTrachDuAn { get; set; }
+        public string? QuanLyHopDong { get; set; }
+        public string? KhachHang { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string? KhuVuc { get; set; }
+        public string? DiaDiem { get; set; }
+        public int? TrangThai { get; set; }
+        public string? GiaiDoan { get; set; }
+        public string? Notes { get; set; }
+        public string? RefrenceFileId { get; set; }
+    }
+}
