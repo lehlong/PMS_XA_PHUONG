cdc29825768809930f2736204e24e2a08a21b30c
Hải Long <hlonn.it@gmail.com>
2025-10-24 14:14:34 +0700

    .

---
+++ Project.Core/Entities/PS/PsProjectWorkflowProcessing.cs
@@ -0,0 +1,21 @@
+﻿using Project.Core.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project.Core.Entities.PS
+{
+    public class PsProjectWorkflowProcessing : BaseEntity
+    {
+        [Key]
+        public string Id { get; set; } = string.Empty;
+        public string ProjectId { get; set; } = string.Empty;
+        public int? Step { get; set; }
+        public string? Name { get; set; }
+        public string? UserName { get; set; }
+        public int? HanXuLy { get; set; }
+        public int? Action { get; set; }
+        public bool? IsDone { get; set; }
+        public bool? IsProcessing { get; set; }
+        public DateTime? Deadline { get; set; }
+        public DateTime? CompletionDate { get; set; }
+    }
+}
---
+++ Project.Service/Dtos/PS/ProjectWorkflowProcessingDto.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Project.Core.Common;
+using Project.Core.Entities.PS;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project.Service.Dtos.PS
+{
+    public class ProjectWorkflowProcessingDto : BaseDto, IMapFrom, IDto
+    {
+        [Key]
+        public string Id { get; set; } = string.Empty;
+        public string ProjectId { get; set; } = string.Empty;
+        public int? Step { get; set; }
+        public string? Name { get; set; }
+        public string? UserName { get; set; }
+        public int? HanXuLy { get; set; }
+        public int? Action { get; set; }
+        public bool? IsDone { get; set; }
+        public bool? IsProcessing { get; set; }
+        public DateTime? Deadline { get; set; }
+        public DateTime? CompletionDate { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<PsProjectWorkflowProcessing, ProjectWorkflowProcessingDto>();
+
+            profile.CreateMap<ProjectWorkflowProcessingDto, PsProjectWorkflowProcessing>();
+        }
+    }
+}
